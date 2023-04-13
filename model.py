0fcf6a1fb7f64954bf5200232b0b674d5c212d17
SunCreation <cake0103@nate.com>
2023-04-13 12:58:49 +0000

    fix: tensor device

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import whisper
 from torch import nn
-from CustomModel import GPT2LMHeadModel
+from CustomModel import GPT2ForTokenClassification
 import torch
 from encoder import TextEncoder, WavEncoder, PrefixEncoder
 from config import Config
@@ -17,12 +17,10 @@ class MetaLM(nn.Module):
         self.text_encoder = TextEncoder(Config.from_dict(eval(config.text_encoder)))
         self.wav_encoder = WavEncoder(Config.from_dict(eval(config.wav_encoder)))
         # GPI is Semi-Causal LM
-        self.GPI = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2',output_hidden_states=True)
+        self.GPI = GPT2ForTokenClassification.from_pretrained('skt/kogpt2-base-v2',output_hidden_states=True,num_labels=config.num_labels)
         self.prompt = config.prompt
         self.classifier = nn.Linear(768,7)
         self.dropout = nn.Dropout2d(config.dropout)
-        self.GPI.transformer.wte = torch.nn.Identity()
-        self.GPI.transformer.wpe = torch.nn.Identity()
         for param in self.GPI.parameters():
             param.requires_grad = False
             
@@ -35,7 +33,7 @@ class MetaLM(nn.Module):
             self.prefix_tokens=torch.arange(self.pre_seq_len).long()
         
     def get_prompt(self, batch_size):
-        prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1)
+        prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(next(self.parameters()).device)
         past_key_values = self.prefix_encoder(prefix_tokens)
         past_key_values = past_key_values.view(
             batch_size,
@@ -59,13 +57,16 @@ class MetaLM(nn.Module):
         """
         batch_size = inputs['text_tokens']['input_ids'].shape[0]
         past_key_values = self.get_prompt(batch_size)
+        labels=inputs['labels']
         text_tokens, wav_tokens = inputs['text_tokens'], inputs['wav_tokens']
         text_tokens = self.text_encoder(text_tokens)
         wav_tokens = self.wav_encoder(wav_tokens)
-        inputs = torch.concat([text_tokens,wav_tokens],dim=1)
+        # fitting seq_len for GPT with prefix
+        inputs = torch.concat([text_tokens,wav_tokens],dim=1)[:,:1019,:]
         # inputs = inputs.view(batch_size,-1,self.n_head,self.n_embd).permute([0,2,1,3])
-        outputs = self.GPI(inputs,past_key_values=past_key_values)
-        pred = self.classifier(outputs[2][11][-1])
-        return pred, outputs
+        outputs = self.GPI(inputs_embeds=inputs,past_key_values=past_key_values,labels=labels)
+        print(outputs.loss)
+        # pred = self.classifier(outputs.hidden_states[:,-1])
+        return outputs
         
         
